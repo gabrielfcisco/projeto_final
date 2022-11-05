89b72ea1fe9dd977fbb035bb63e48b2d2e06b96a
EduardoSilvaS <eduardoss2020.ve@gmail.com>
2022-11-04 22:34:00 -0300

    Atualizacao Migration

---
+++ database/migrations/2022_11_02_223634_create_professores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professores', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nome');
+            $table->string('CPF');
+            $table->string('Endereco');
+            $table->string('User');
+            $table->string('Password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('professores');
+    }
+};
