e9dc62ba4483bb4f3474fb79fd10d0c45ff256e4
gabrielfcisco <gabrifran017@gmail.com>
2022-11-22 09:39:40 -0300

    fix: variáveis na blade edit de alunos

--- resources/views/secretaria/aluno/edit.blade.php
+++ resources/views/secretaria/aluno/edit.blade.php
@@ -1,11 +1,11 @@
-@extends('alunos.master')
+@extends('layouts.app')
 @section('content')
 <div class="row">
 	<div class="col-12 col-md-10">
 		<h3>Editar Aluno</h3>
 	</div>
 	<div class="col-12 col-md-2 text-end">
-		<a class="btn btn-primary" href="{{route('alunos.index')}}">Voltar</a>
+		<a class="btn btn-primary" href="{{route('aluno.index')}}">Voltar</a>
 	</div>
 </div>
 @if($errors->any())
@@ -25,24 +25,24 @@
 @endif
 <div class="row">
 	<div class="col-12">
-		<form method="POST" action="{{route('alunos.update', $aluno->id)}}">
+		<form method="POST" action="{{route('aluno.update', $aluno->id)}}">
 			@csrf
 			@method('PUT')
 			<div class="mb-3">
 			    <label for="RA" class="form-label">RA</label>
-			    <input type="text" class="form-control" id="RA" name="RA" value="{{$aluno->RA}}">
+			    <input type="text" class="form-control" id="RA" name="RA" value="{{$aluno->email}}">
 		  	</div>
 			<div class="mb-3">
 			    <label for="Nome" class="form-label">Nome</label>
-			    <input type="text" class="form-control" id="Nome" name="Nome" value="{{$aluno->Nome}}">
+			    <input type="text" class="form-control" id="Nome" name="Nome" value="{{$aluno->nome}}">
 		  	</div>
 		  	<div class="mb-3">
 			    <label for="Sobrenome" class="form-label">Sobrenome</label>
-			    <input type="text" class="form-control" id="Sobrenome" name="Sobrenome" value="{{$aluno->Sobrenome}}">
+			    <input type="text" class="form-control" id="Sobrenome" name="Sobrenome" value="{{$aluno->CPF}}">
 		  	</div>
 			<div class="mb-3">
 				<label for="Filmes" class="form-label">Filmes</label><br>
-				<select class="filmes" name="Filmes[]" multiple="multiple" id="Filmes" value="{{$aluno->Filmes}}">
+				<select class="filmes" name="Filmes[]" multiple="multiple" id="Filmes" value="{{$aluno->filmes}}">
 					@if(count($filmes) > 0)
 						@foreach($filmes as $filme)
 						<option value="{{ $filme['nome'] }}">{{ $filme['nome'] }}</option>
@@ -60,8 +60,8 @@
 			<div class="mb-3">
 				<label for="id_materia" class="form-label">Matérias</label><br>
 				<select name="id_materia" class="id_materia" id="id_materia">
-					@if($materias->count() > 0)
-					@foreach($materias as $materia)
+					@if($curso->count() > 0)
+					@foreach($curso as $materia)
 					<option value="{{ $materia['id'] }}">{{ $materia['Nome'] }}</option>
 					@endforeach
 					@else
